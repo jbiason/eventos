9166d47aa1a3b50f44fffd9061f37486fbf1972b
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-07 10:41:41 -0200

    Watch test files to run tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -48,6 +48,7 @@ gulp.task('test', () => {
 
 gulp.task('watch', () => {
   gulp.watch(config.testsGlob, ['lint:tests']);
+  gulp.watch(config.testsGlob, ['test']);
   gulp.watch(config.scriptsGlob, ['lint:scripts']);
   gulp.watch(config.scriptsGlob, ['test']);
 });
