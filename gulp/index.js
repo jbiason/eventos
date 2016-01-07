f15545a149ba6362a5a16d6f4d4c67d263b2ebc3
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-07 17:59:32 -0200

    Expand npm scripts

--- gulp/index.js
+++ gulp/index.js
@@ -3,7 +3,8 @@ import requireDir from 'require-dir';
 
 requireDir('./tasks');
 
+gulp.task('qa', ['lint', 'test']);
+gulp.task('ci', ['test']);
 gulp.task('dev', ['lint', 'test', 'webpack:dev', 'watch']);
 gulp.task('build', ['webpack:build'])
 gulp.task('pre-commit', ['lint', 'test', 'build']);
-gulp.task('ci', ['test']);
