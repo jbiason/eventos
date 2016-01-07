a86bba50f82c57b8c1b1f81f593781c8df834791
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-07 17:10:55 -0200

    Add error handler to lint task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -4,20 +4,25 @@ import eslint from 'gulp-eslint';
 import config from '../config';
 import env from '../env';
 
-gulp.task('lint:scripts', () => {
-  return gulp.src(config.scriptsGlob)
+const linterStream = (glob) => {
+  return gulp.src(glob)
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(env.isPreCommit() ? eslint.failAfterError() : gutil.noop())
+  .on('error', () => {
+    if (env.isPreCommit()) {
+      process.exit(1);
+    }
+  })
   ;
+};
+
+gulp.task('lint:scripts', () => {
+  return linterStream(config.scriptsGlob);
 });
 
 gulp.task('lint:tests', () => {
-  return gulp.src(config.testsGlob)
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(env.isPreCommit() ? eslint.failAfterError() : gutil.noop())
-  ;
+  return linterStream(config.testsGlob);
 });
 
 gulp.task('lint', ['lint:scripts', 'lint:tests']);
