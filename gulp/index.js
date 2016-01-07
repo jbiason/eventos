1bc378da4023c6dedbb84bd67c0d216e5757b0ec
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-07 20:19:23 -0200

    Refactor gulp tasks and npm scripts

--- gulp/index.js
+++ gulp/index.js
@@ -3,8 +3,13 @@ import requireDir from 'require-dir';
 
 requireDir('./tasks');
 
-gulp.task('qa', ['lint', 'test']);
 gulp.task('ci', ['test']);
-gulp.task('build', ['del', 'copy', 'webpack:build']);
-gulp.task('pre-commit', ['lint', 'test', 'build']);
-gulp.task('dev', ['lint', 'test', 'del', 'copy', 'webpack:dev', 'watch']);
+gulp.task('qa', ['lint', 'test']);
+
+gulp.task('build-prod', ['copy', 'webpack:build']);
+// gulp.task('build-prod', ['del', 'copy', 'webpack:build']); // use this when we don't commit bundle anymore
+gulp.task('pre-commit', ['lint', 'test', 'build-prod']);
+
+gulp.task('build-dev', ['copy', 'webpack:dev']);
+// gulp.task('build-dev', ['del', 'copy', 'webpack:dev']); // use this when we don't commit bundle anymore
+gulp.task('dev', ['lint', 'test', 'build-dev', 'watch']);
