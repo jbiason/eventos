87387007dea45491bee186632a630cd3a2020738
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-07 15:19:19 -0200

    Test CI

--- gulp/index.js
+++ gulp/index.js
@@ -5,5 +5,5 @@ requireDir('./tasks');
 
 gulp.task('dev', ['lint', 'test', 'webpack:dev', 'watch']);
 gulp.task('build', ['webpack:build'])
-gulp.task('ci', ['lint', 'test', 'build']);
 gulp.task('pre-commit', ['lint', 'test', 'build']);
+gulp.task('ci', ['test']);
