73036449f6ed9189661ef07738e9e9e5c84bc338
Rafael Eyng <rafaeleyng@gmail.com>
2016-01-08 16:15:44 -0200

    Fix test to test coverage badge

--- test/components/Common/Radio.js
+++ test/components/Common/Radio.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import Radio from '../../../src/js/components/Common/Radio';
 
 it('should render radio', () => {
-  const wrapper = shallow(<Radio />);
+  const wrapper = shallow(<Radio text="some text" val={1} />);
   expect(wrapper.find('.ev-select__icon')).to.have.length.of(1);
   expect(wrapper.find('.ev-select__text')).to.have.length.of(1);
 });
